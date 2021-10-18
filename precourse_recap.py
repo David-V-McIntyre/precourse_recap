1ed63fb36385537707f324733f5204027d7149bb
David McIntyre <david.v.mcintyre@gmail.com>
2021-10-18 16:44:03 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,10 @@
+print("Guess my favourite number!")
+
+fav_number = input("What is my favourite number? ")
+print(fav_number)
+
+if fav_number == 7:
+    print("Well done!")
+
+else:
+    print("Sorry, Try again!")
