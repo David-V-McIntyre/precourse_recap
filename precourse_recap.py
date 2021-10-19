ddb02597e61318629c70a862e5866f3f9e394dc5
David McIntyre <david.v.mcintyre@gmail.com>
2021-10-19 09:45:46 +0100

    aminor text fixes

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,5 +1,3 @@
-
-
 fav_number = input("What is my favourite number? Careful, you only have 1 life left! ")
 print(fav_number)
 
@@ -7,4 +5,4 @@ if fav_number == 7:
     print("Well done! You still have 1 life left.")
 
 else:
-    print("That is incorrect. You have failed me. You have 0 lives left. Game Over")
+    print("That is incorrect. You have failed me. You have 0 lives left. Game Over.")
