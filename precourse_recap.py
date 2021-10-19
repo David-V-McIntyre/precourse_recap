ea727f31c26fe82dae0226f04d251822137c59a6
David McIntyre <david.v.mcintyre@gmail.com>
2021-10-19 09:34:34 +0100

    changed text to reflect lives

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,10 +1,10 @@
-print("Guess my favourite number!")
 
-fav_number = input("What is my favourite number? ")
+
+fav_number = input("What is my favourite number? Careful, you only have 1 life left! ")
 print(fav_number)
 
 if fav_number == 7:
-    print("Well done!")
+    print("Well done! You still have 1 life left.")
 
 else:
-    print("Sorry, Try again!")
+    print("That is incorrect. You have failed me. You have 0 lives left. Game Over")
